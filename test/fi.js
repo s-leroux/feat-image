21ce29624d10aa1648d4039cd81d1a88678dd028
Sylvain Leroux <sylvain@chicoree.fr>
2022-07-08 00:14:08 +0200

    Parse og:image:height and og:image:width

--- test/fi.js
+++ test/fi.js
@@ -9,5 +9,26 @@ describe("feat-image", () => {
       .then((imageurl) => assert.deepEqual(imageurl, ["https://nodejs.org/static/images/logo-hexagon-card.png"]));
   });
 
+  it("should parse og:image:height and og:image:width", () => {
+    return fi.about(`http://${config.server}/Architecture`)
+      .then((about) => assert.deepEqual(about.fi, [
+        {
+          "href": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg/1200px-Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg",
+          "width": 1200,
+          "height": 836,
+        },
+        {
+          "href": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg/800px-Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg",
+          "width": 800,
+          "height": 557,
+        },
+        {
+          "href": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg/640px-Santuario_de_Las_Lajas%2C_Ipiales%2C_Colombia%2C_2015-07-21%2C_DD_21-23_HDR.jpg",
+          "width": 640,
+          "height": 446,
+        },
+      ]));
+  });
+
 
 });
